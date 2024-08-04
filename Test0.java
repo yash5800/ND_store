4bc95ca7812847bef0855061f692653092041a59
A.Yaswanth Kiran <113412043+yash5800@users.noreply.github.com>
2024-08-04 13:29:54 +0530

    Add file via Flask

---
+++ Test0.java
@@ -0,0 +1,8 @@
+class Test0 {    
+    float a =10.8699;
+    public static void main(String[] args) {
+        Test t = new Test();
+        // System.out.println("hello");
+        System.out.printf("%.2f", t.a);
+    }
+}
