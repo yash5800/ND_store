069ac3e6c77ad798ce8da16176254ab3feb42f0b
A.Yaswanth Kiran <113412043+yash5800@users.noreply.github.com>
2024-11-11 16:06:07 +0530

    Adding chunk 1 of 1

---
+++ 1.sql
@@ -0,0 +1,10 @@
+CREATE TABLE user5 (uname VARCHAR(20),pwd INT);
+
+
+INSERT INTO user5(uname,pwd) values('CSE','123');
+
+
+INSERT INTO user5(uname,pwd) values('SRGEC','456');
+
+
+select * from user5;
